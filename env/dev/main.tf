267e65feaad797b10e7653e47b6897c84014d8c8
simone84 <simone84.an@gmail.com>
2023-02-20 14:16:00 +0400

    labmda tf file

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -20,13 +20,3 @@ terraform {
 variable "aws_region" {}
 variable "python_v" {}
 variable "env" {}
-
-// LAMBDA NAME: instances_id //
-
-module "lambda-instances_id" {
-  source = "../../modules/lambda-ondemand"
-
-  env = var.env
-  lambda_name = "instances_id"
-  python_v = var.python_v
-}
