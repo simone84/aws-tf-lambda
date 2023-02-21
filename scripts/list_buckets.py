7d5388f506da311b6f0461e2fe0280182ee680a9
simone84 <simone84.an@gmail.com>
2023-02-21 12:19:05 +0400

    loop to print name only

--- scripts/list_buckets.py
+++ scripts/list_buckets.py
@@ -1,10 +1,12 @@
+import json
 import boto3
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     client = boto3.client('s3')
     response = client.list_buckets()
-    print (response)
+    for metadata in response['Buckets']:
+        print(metadata['Name'])
     
     return {
         'body': json.dumps('Done')
